77a2a52fca8a2c018ec5aa32438db96359249f1b
takusan23 <32033405+takusan23@users.noreply.github.com>
2018-12-18 16:31:06 +0900

    ウィジェット！！！！

---
+++ app/src/main/java/com/takusan_23/kaisendon/WidgetService.java
@@ -0,0 +1,11 @@
+package com.takusan_23.kaisendon;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class WidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return null;
+    }
+}

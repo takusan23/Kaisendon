18008c6da00ea71fef438ec940f9b94640b86215
takusan23 <takumu1200@gmail.com>
2018-12-19 01:49:42 +0900

    ウィジェットできた。。。

--- app/src/main/java/com/takusan_23/kaisendon/NewAppWidget.java
+++ app/src/main/java/com/takusan_23/kaisendon/NewAppWidget.java
@@ -1,67 +1,192 @@
 package com.takusan_23.kaisendon;
 
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.RemoteInput;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.sys1yagi.mastodon4j.MastodonClient;
+import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException;
+import com.sys1yagi.mastodon4j.api.method.Statuses;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        //views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        //ブロードキャストを送信
-        //ブロードキャスト先を指定（明示的ブロードキャスト）
-        //マニフェストにも記入しないと動かないので注意
-
-
-
-/*
-        //ローカルトースト
-        Intent toot_Broadcast = new Intent(context, BroadcastReciver_Widget_ToastTimeline.class);
-        //アカウントペーじ
-        Intent account_Intent = new Intent(context, AccountActivity.class);
-        //トゥート
-        Intent toot_Intent = new Intent(context, TootActivity.class);
-        //ボタン
-        views.setOnClickPendingIntent(R.id.widegt_1, PendingIntent.getActivity(context, 0, account_Intent, PendingIntent.FLAG_UPDATE_CURRENT));
-        views.setOnClickPendingIntent(R.id.widegt_2, PendingIntent.getActivity(context, 0, toot_Intent, PendingIntent.FLAG_UPDATE_CURRENT));
-        views.setOnClickPendingIntent(R.id.widegt_3, PendingIntent.getBroadcast(context, 0, toot_Broadcast, PendingIntent.FLAG_UPDATE_CURRENT));
-*/
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
+    private static final String ACTION_CLICK = "kaisendon.widget.Public";
+    SharedPreferences pref_setting;
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
+    public void onUpdate(Context ctx, AppWidgetManager manager, int[] appWidgetIds) {
+        super.onUpdate(ctx, manager, appWidgetIds);
+
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            Intent remoteViewsFactoryIntent = new Intent(ctx, WidgetService.class);
+            RemoteViews rv = new RemoteViews(ctx.getPackageName(), R.layout.new_app_widget);
+            rv.setRemoteAdapter(R.id.widget_listview, remoteViewsFactoryIntent);
 
-            Intent remoteViewsFactoryIntent = new Intent(context,WidgetService.class);
+            setOnButtonClickPendingIntent_Load(ctx, rv, appWidgetId);
+            setOnButtonClickPendingIntent_Toot(ctx, rv, appWidgetId);
 
+            manager.updateAppWidget(appWidgetId, rv);
         }
     }
 
     @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
+    public void onReceive(Context ctx, Intent intent) {
+        super.onReceive(ctx, intent);
+
+        int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        if (appWidgetId != 0) {
+            AppWidgetManager.getInstance(ctx).notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_listview);
+
+        }
+
+        if (intent.getBooleanExtra("TootMode", false)) {
+
+            Intent notification_localtimeline_toot = new Intent(ctx, NewAppWidget.class);
+            PendingIntent notification_localtimeline_pendingIntent = PendingIntent.getBroadcast(ctx, 1, notification_localtimeline_toot, PendingIntent.FLAG_UPDATE_CURRENT);
+            //トゥート
+            android.support.v4.app.RemoteInput remoteInput = new android.support.v4.app.RemoteInput.Builder("Toot_Text")
+                    .setLabel("今なにしてる？")
+                    .build();
+
+            NotificationCompat.Action notification_toot_action = new NotificationCompat.Action.Builder(android.R.drawable.ic_menu_send
+                    , "今なにしてる？", notification_localtimeline_pendingIntent)
+                    .addRemoteInput(remoteInput)
+                    .build();
+
+            long[] pattern = {100};
+
+            Notification newMessageNotification =
+                    new NotificationCompat.Builder(ctx)
+                            .setSmallIcon(R.drawable.ic_create_black_24dp_black)
+                            .setContentTitle(ctx.getString(R.string.toot))
+                            .setContentText(ctx.getString(R.string.imananisiteru))
+                            .setPriority(1)
+                            .setVibrate(pattern)
+                            .addAction(notification_toot_action).build();
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(ctx);
+            notificationManager.notify(0, newMessageNotification);
+        }
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (remoteInput != null) {
+            CharSequence charSequence = "";
+            NotificationManager notificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            if (remoteInput != null) {
+                charSequence = remoteInput.getCharSequence("Toot_Text");
+                System.out.println("Reply : " + (String) charSequence);
+
+                CharSequence finalCharSequence = charSequence;
+                new AsyncTask<String, Void, String>() {
+                    @Override
+                    protected String doInBackground(String... string) {
+                        try {
+                            pref_setting = PreferenceManager.getDefaultSharedPreferences(Preference_ApplicationContext.getContext());
+
+                            String AccessToken, Instance;
+
+                            boolean accessToken_boomelan = pref_setting.getBoolean("pref_advanced_setting_instance_change", false);
+                            if (accessToken_boomelan) {
+                                AccessToken = pref_setting.getString("pref_mastodon_accesstoken", "");
+                                Instance = pref_setting.getString("pref_mastodon_instance", "");
+                            } else {
+                                AccessToken = pref_setting.getString("main_token", "");
+                                Instance = pref_setting.getString("main_instance", "");
+                            }
+                            MastodonClient client = new MastodonClient.Builder(Instance, new OkHttpClient.Builder(), new Gson()).accessToken(AccessToken).build();
+                            com.sys1yagi.mastodon4j.api.entity.Status statuses = new Statuses(client).postStatus((String) finalCharSequence, null, null, false, null).execute();
+                        } catch (Mastodon4jRequestException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
+                Toast.makeText(ctx, ctx.getString(R.string.toot_ok) + " : " + charSequence, Toast.LENGTH_SHORT).show();
+                notificationManager.cancel(R.string.notification_LocalTimeline_Notification);
+            }
+        }
+/*
+
+        if (intent.getBooleanExtra("Open", false)) {
+            Intent openApp = new Intent(ctx, Home.class);
+            if (pref_setting.getString("WidgetTLType", "Home").contains("Home")) {
+                openApp.putExtra("Home", true);
+                ctx.startActivity(openApp);
+            }
+            if (pref_setting.getString("WidgetTLType", "Home").contains("Notification")) {
+                openApp.putExtra("Notification", true);
+                ctx.startActivity(openApp);
+            }
+            if (pref_setting.getString("WidgetTLType", "Home").contains("Local")) {
+                openApp.putExtra("Local", true);
+                ctx.startActivity(openApp);
+            }
+            if (pref_setting.getString("WidgetTLType", "Home").contains("Federated")) {
+                openApp.putExtra("Federated", true);
+                ctx.startActivity(openApp);
+            }
+            openApp.putExtra("Home", true);
+            ctx.startActivity(openApp);
+
+*/
+
+
     }
 
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
+    private void setOnButtonClickPendingIntent_Load(Context ctx, RemoteViews rv, int appWidgetId) {
+        Intent btnClickIntent = new Intent(ctx, NewAppWidget.class);
+        btnClickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+
+        PendingIntent btnClickPendingIntent = PendingIntent.getBroadcast(
+                ctx,
+                23,
+                btnClickIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        rv.setOnClickPendingIntent(R.id.widget_button_load, btnClickPendingIntent);
     }
+
+    private void setOnButtonClickPendingIntent_Toot(Context ctx, RemoteViews rv, int appWidgetId) {
+        Intent btnClickIntent = new Intent(ctx, NewAppWidget.class);
+        btnClickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        btnClickIntent.putExtra("TootMode", true);
+
+        PendingIntent btnClickPendingIntent = PendingIntent.getBroadcast(
+                ctx,
+                24,
+                btnClickIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        rv.setOnClickPendingIntent(R.id.widget_button_toot, btnClickPendingIntent);
+    }
+
 }
-

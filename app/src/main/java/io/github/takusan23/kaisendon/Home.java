f032fc09962e79efd7033e4bd61ebe05d008dd6b
takusan23 <takumu1200@gmail.com>
2019-03-30 00:50:36 +0900

    片手モード、ダークモード対応

--- app/src/main/java/io/github/takusan23/kaisendon/Home.java
+++ app/src/main/java/io/github/takusan23/kaisendon/Home.java
@@ -9,6 +9,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.app.TaskStackBuilder;
+import android.app.UiModeManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -2291,60 +2292,103 @@ public class Home extends AppCompatActivity
                 null,
                 null
         );
+
+        String name = "";
+        String content = "";
+        String instance = "";
+        String access_token = "";
+        String image_load = "";
+        String dialog = "";
+        String dark_mode = "";
+        String position = "";
+        String streaming = "";
+        String subtitle = "";
+        String image_url = "";
+        String background_transparency = "";
+        String background_screen_fit = "";
+        String quick_profile = "";
+        String toot_counter = "";
+        String custom_emoji = "";
+        String gif = "";
+        String font = "";
+        String one_hand = "";
+        String setting = "";
+
         cursor.moveToFirst();
         for (int i = 0; i < cursor.getCount(); i++) {
             try {
                 JSONObject jsonObject = new JSONObject(cursor.getString(1));
-                String name = jsonObject.getString("name");
-                String content = jsonObject.getString("content");
-                String instance = jsonObject.getString("instance");
-                String access_token = jsonObject.getString("access_token");
-                String image_load = jsonObject.getString("image_load");
-                String dialog = jsonObject.getString("dialog");
-                String dark_mode = jsonObject.getString("dark_mode");
-                String position = jsonObject.getString("position");
-                String streaming = jsonObject.getString("streaming");
-                String subtitle = jsonObject.getString("subtitle");
-                String image_url = jsonObject.getString("image_url");
-                String background_transparency = jsonObject.getString("background_transparency");
-                String background_screen_fit = jsonObject.getString("background_screen_fit");
-                String quick_profile = jsonObject.getString("quick_profile");
-                String toot_counter = jsonObject.getString("toot_counter");
-                String custom_emoji = jsonObject.getString("custom_emoji");
-                String gif = jsonObject.getString("gif");
-                String font = jsonObject.getString("font");
-                String one_hand = jsonObject.getString("one_hand");
-                String setting = jsonObject.getString("setting");
+                name = jsonObject.getString("name");
+                content = jsonObject.getString("content");
+                instance = jsonObject.getString("instance");
+                access_token = jsonObject.getString("access_token");
+                image_load = jsonObject.getString("image_load");
+                dialog = jsonObject.getString("dialog");
+                dark_mode = jsonObject.getString("dark_mode");
+                position = jsonObject.getString("position");
+                streaming = jsonObject.getString("streaming");
+                subtitle = jsonObject.getString("subtitle");
+                image_url = jsonObject.getString("image_url");
+                background_transparency = jsonObject.getString("background_transparency");
+                background_screen_fit = jsonObject.getString("background_screen_fit");
+                quick_profile = jsonObject.getString("quick_profile");
+                toot_counter = jsonObject.getString("toot_counter");
+                custom_emoji = jsonObject.getString("custom_emoji");
+                gif = jsonObject.getString("gif");
+                font = jsonObject.getString("font");
+                one_hand = jsonObject.getString("one_hand");
+                setting = jsonObject.getString("setting");
 
                 //メニュー追加
+                String finalName = name;
+                String finalContent = content;
+                String finalInstance = instance;
+                String finalAccess_token = access_token;
+                String finalImage_load = image_load;
+                String finalDialog = dialog;
+                String finalDark_mode = dark_mode;
+                String finalPosition = position;
+                String finalStreaming = streaming;
+                String finalSubtitle = subtitle;
+                String finalImage_url = image_url;
+                String finalBackground_transparency = background_transparency;
+                String finalBackground_screen_fit = background_screen_fit;
+                String finalQuick_profile = quick_profile;
+                String finalToot_counter = toot_counter;
+                String finalCustom_emoji = custom_emoji;
+                String finalGif = gif;
+                String finalFont = font;
+                String finalOne_hand = one_hand;
+                String finalSetting = setting;
                 navigationView.getMenu().add(name).setIcon(urlToContent(content)).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
                         //Fragment切り替え
                         //受け渡す
                         Bundle bundle = new Bundle();
-                        bundle.putString("name", name);
-                        bundle.putString("content", content);
-                        bundle.putString("instance", instance);
-                        bundle.putString("access_token", access_token);
-                        bundle.putString("image_load", image_load);
-                        bundle.putString("dialog", dialog);
-                        bundle.putString("dark_mode", dark_mode);
-                        bundle.putString("position", position);
-                        bundle.putString("streaming", streaming);
-                        bundle.putString("subtitle", subtitle);
-                        bundle.putString("image_url", image_url);
-                        bundle.putString("background_transparency", background_transparency);
-                        bundle.putString("background_screen_fit", background_screen_fit);
-                        bundle.putString("quick_profile", quick_profile);
-                        bundle.putString("toot_counter", toot_counter);
-                        bundle.putString("custom_emoji", custom_emoji);
-                        bundle.putString("gif", gif);
-                        bundle.putString("font", font);
-                        bundle.putString("one_hand", one_hand);
-                        bundle.putString("setting", setting);
+                        bundle.putString("name", finalName);
+                        bundle.putString("content", finalContent);
+                        bundle.putString("instance", finalInstance);
+                        bundle.putString("access_token", finalAccess_token);
+                        bundle.putString("image_load", finalImage_load);
+                        bundle.putString("dialog", finalDialog);
+                        bundle.putString("dark_mode", finalDark_mode);
+                        bundle.putString("position", finalPosition);
+                        bundle.putString("streaming", finalStreaming);
+                        bundle.putString("subtitle", finalSubtitle);
+                        bundle.putString("image_url", finalImage_url);
+                        bundle.putString("background_transparency", finalBackground_transparency);
+                        bundle.putString("background_screen_fit", finalBackground_screen_fit);
+                        bundle.putString("quick_profile", finalQuick_profile);
+                        bundle.putString("toot_counter", finalToot_counter);
+                        bundle.putString("custom_emoji", finalCustom_emoji);
+                        bundle.putString("gif", finalGif);
+                        bundle.putString("font", finalFont);
+                        bundle.putString("one_hand", finalOne_hand);
+                        bundle.putString("setting", finalSetting);
                         CustomMenuTimeLine customMenuTimeLine = new CustomMenuTimeLine();
                         customMenuTimeLine.setArguments(bundle);
+
 
                         //置き換え
                         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -2355,6 +2399,65 @@ public class Home extends AppCompatActivity
                 });
             } catch (JSONException e) {
                 e.printStackTrace();
+                //なくてもとりあえず追加する
+                //メニュー追加
+                String finalName = name;
+                String finalContent = content;
+                String finalInstance = instance;
+                String finalAccess_token = access_token;
+                String finalImage_load = image_load;
+                String finalDialog = dialog;
+                String finalDark_mode = dark_mode;
+                String finalPosition = position;
+                String finalStreaming = streaming;
+                String finalSubtitle = subtitle;
+                String finalImage_url = image_url;
+                String finalBackground_transparency = background_transparency;
+                String finalBackground_screen_fit = background_screen_fit;
+                String finalQuick_profile = quick_profile;
+                String finalToot_counter = toot_counter;
+                String finalCustom_emoji = custom_emoji;
+                String finalGif = gif;
+                String finalFont = font;
+                String finalOne_hand = one_hand;
+                String finalSetting = setting;
+                navigationView.getMenu().add(name).setIcon(urlToContent(content)).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        //Fragment切り替え
+                        //受け渡す
+                        Bundle bundle = new Bundle();
+                        bundle.putString("name", finalName);
+                        bundle.putString("content", finalContent);
+                        bundle.putString("instance", finalInstance);
+                        bundle.putString("access_token", finalAccess_token);
+                        bundle.putString("image_load", finalImage_load);
+                        bundle.putString("dialog", finalDialog);
+                        bundle.putString("dark_mode", finalDark_mode);
+                        bundle.putString("position", finalPosition);
+                        bundle.putString("streaming", finalStreaming);
+                        bundle.putString("subtitle", finalSubtitle);
+                        bundle.putString("image_url", finalImage_url);
+                        bundle.putString("background_transparency", finalBackground_transparency);
+                        bundle.putString("background_screen_fit", finalBackground_screen_fit);
+                        bundle.putString("quick_profile", finalQuick_profile);
+                        bundle.putString("toot_counter", finalToot_counter);
+                        bundle.putString("custom_emoji", finalCustom_emoji);
+                        bundle.putString("gif", finalGif);
+                        bundle.putString("font", finalFont);
+                        bundle.putString("one_hand", finalOne_hand);
+                        bundle.putString("setting", finalSetting);
+                        CustomMenuTimeLine customMenuTimeLine = new CustomMenuTimeLine();
+                        customMenuTimeLine.setArguments(bundle);
+
+
+                        //置き換え
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.container_container, customMenuTimeLine);
+                        transaction.commit();
+                        return false;
+                    }
+                });
             }
 
             cursor.moveToNext();

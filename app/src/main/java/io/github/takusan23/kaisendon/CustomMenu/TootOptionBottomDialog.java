4a028a9511f124632ea08e845589fbbb59f6c300
takusan23 <takumu1200@gmail.com>
2019-04-16 03:35:18 +0900

    予約投稿機能つける

--- app/src/main/java/io/github/takusan23/kaisendon/CustomMenu/TootOptionBottomDialog.java
+++ app/src/main/java/io/github/takusan23/kaisendon/CustomMenu/TootOptionBottomDialog.java
@@ -1,26 +1,48 @@
 package io.github.takusan23.kaisendon.CustomMenu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import io.github.takusan23.kaisendon.Activity.UserActivity;
 import io.github.takusan23.kaisendon.R;
 
 public class TootOptionBottomDialog extends BottomSheetDialogFragment {
 
+    private View view;
+    private TextView account_Button;
+    private TextView bookmark_Button;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return View.inflate(getContext(), R.layout.toot_option_button_dialog_layout, null);
+        return inflater.inflate(R.layout.toot_option_button_dialog_layout, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        account_Button = view.findViewById(R.id.toot_option_account_button);
+        bookmark_Button= view.findViewById(R.id.toot_option_bookmark_button);
+        //クリック
+        account_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(account_Button.getContext(), UserActivity.class);
+                //IDを渡す
+                if (CustomMenuTimeLine.isMisskeyMode()){
+                    intent.putExtra("Misskey", true);
+                }
+                intent.putExtra("Account_ID", getArguments().getString("user_id"));
+                account_Button.getContext().startActivity(intent);
+
+            }
+        });
     }
 }

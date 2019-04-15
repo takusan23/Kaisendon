cf8b7358054e995260500d795e852362fda9beb9
takusan23 <32033405+takusan23@users.noreply.github.com>
2019-04-15 12:13:26 +0900

    メニュー作る

---
+++ app/src/main/java/io/github/takusan23/kaisendon/CustomMenu/TootOptionBottomDialog.java
@@ -0,0 +1,26 @@
+package io.github.takusan23.kaisendon.CustomMenu;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.github.takusan23.kaisendon.R;
+
+public class TootOptionBottomDialog extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return View.inflate(getContext(), R.layout.toot_option_button_dialog_layout, null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}

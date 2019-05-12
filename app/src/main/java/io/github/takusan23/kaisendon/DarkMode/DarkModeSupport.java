35f0eb473dc64752bc12b7f7944a2b6f10a2110e
takusan23 <takumu1200@gmail.com>
2019-05-12 20:14:40 +0900

    文字に色をつける機能を追加した

--- app/src/main/java/io/github/takusan23/kaisendon/DarkMode/DarkModeSupport.java
+++ app/src/main/java/io/github/takusan23/kaisendon/DarkMode/DarkModeSupport.java
@@ -9,8 +9,10 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.preference.PreferenceManager;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -136,6 +138,9 @@ public class DarkModeSupport {
             if (layout.getChildAt(i) instanceof LinearLayout) {
                 setLayoutAllThemeColor((LinearLayout) layout.getChildAt(i));
             }
+            if (layout.getChildAt(i) instanceof EditText) {
+                setEditTextThemeColor((EditText) layout.getChildAt(i));
+            }
             if (layout.getChildAt(i) instanceof TextView) {
                 setTextViewThemeColor((TextView) layout.getChildAt(i));
             }
@@ -144,6 +149,9 @@ public class DarkModeSupport {
             }
             if (layout.getChildAt(i) instanceof BottomNavigationView) {
                 setBottomNavigationBerThemeColor((BottomNavigationView) layout.getChildAt(i));
+            }
+            if (layout.getChildAt(i) instanceof TextInputLayout) {
+                setTextInputLayoutThemeColor((TextInputLayout) layout.getChildAt(i));
             }
 
         }
@@ -174,6 +182,37 @@ public class DarkModeSupport {
                 view.setItemBackgroundResource(R.color.black);
                 view.setItemTextColor(ColorStateList.valueOf(context.getResources().getColor(R.color.white, null)));
                 view.setItemIconTintList(null);
+                break;
+        }
+    }
+
+
+    /*TextInoutLayout*/
+    public void setTextInputLayoutThemeColor(TextInputLayout view) {
+        switch (nightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                view.setBoxStrokeColor(context.getResources().getColor(R.color.white, null));
+                view.setBoxBackgroundColor(context.getResources().getColor(R.color.white, null));
+                view.setBoxBackgroundMode(context.getResources().getColor(R.color.white, null));
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                view.setBoxStrokeColor(context.getResources().getColor(R.color.black, null));
+                view.setBoxBackgroundColor(context.getResources().getColor(R.color.black, null));
+                view.setBoxBackgroundMode(context.getResources().getColor(R.color.black, null));
+                break;
+        }
+    }
+
+    /*EditText*/
+    public void setEditTextThemeColor(EditText view) {
+        switch (nightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                view.setHintTextColor(context.getResources().getColor(android.R.color.darker_gray, context.getTheme()));
+                view.setBackgroundTintList(context.getResources().getColorStateList(android.R.color.black, context.getTheme()));
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                view.setHintTextColor(context.getResources().getColor(android.R.color.darker_gray, context.getTheme()));
+                view.setBackgroundTintList(context.getResources().getColorStateList(android.R.color.white, context.getTheme()));
                 break;
         }
     }

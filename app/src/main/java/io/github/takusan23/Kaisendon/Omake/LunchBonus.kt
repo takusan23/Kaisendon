9d7f7e84d051217cab512c3fcbf5a75ff61dc10e
takusan23 <takumu1200@gmail.com>
2019-07-01 22:09:36 +0900

    6.0.0 製品版公開です

--- app/src/main/java/io/github/takusan23/Kaisendon/Omake/LunchBonus.kt
+++ app/src/main/java/io/github/takusan23/Kaisendon/Omake/LunchBonus.kt
@@ -24,7 +24,8 @@ class LunchBonus(private val context: Context) {
             //前回の記録から一日経過したらカウント
             var count = Integer.valueOf(pref_setting.getString("lunch_count", "0")!!)
             val yesterday = Integer.valueOf(pref_setting.getString("lunch_day", "0")!!)
-            if (calendar.get(Calendar.DATE) > yesterday) {
+            //これ月変わったら１から始まるから雑分岐
+            if (calendar.get(Calendar.DATE) != yesterday) {
                 //更新
                 count++
                 editor.putString("lunch_count", count.toString())
@@ -38,5 +39,4 @@ class LunchBonus(private val context: Context) {
         editor.putString("lunch_day", calendar.get(Calendar.DATE).toString())
         editor.apply()
     }
-
 }

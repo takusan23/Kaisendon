674a74c919635bb0c2a1e836655e1943702fe05f
takusan23 <32033405+takusan23@users.noreply.github.com>
2019-09-04 22:43:45 +0900

    時間指定投稿をまず実装？

--- app/src/main/java/io/github/takusan23/Kaisendon/TootCardView.kt
+++ app/src/main/java/io/github/takusan23/Kaisendon/TootCardView.kt
@@ -1,17 +1,22 @@
 package io.github.takusan23.Kaisendon
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Context
 import android.os.BatteryManager
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.view.menu.MenuBuilder
 import androidx.appcompat.view.menu.MenuPopupHelper
 import androidx.cardview.widget.CardView
+import kotlinx.android.synthetic.main.activity_toot.view.*
 import kotlinx.android.synthetic.main.carview_toot_layout.view.*
+import java.util.*
 
 class TootCardView(val context: Context,val isMisskey:Boolean){
 
@@ -26,7 +31,28 @@ class TootCardView(val context: Context,val isMisskey:Boolean){
         laytoutInflater.inflate(R.layout.carview_toot_layout,linearLayout)
         tootEditText = linearLayout.toot_card_textinput
     }
-    
+
+    fun setClickEvent(){
+        linearLayout.toot_card_attach_image.setOnClickListener {
+            setAttachImage()
+        }
+        linearLayout.device_info_button.setOnClickListener {
+            showDeviceInfo()
+        }
+        linearLayout.toot_card_visibility_button.setOnClickListener {
+            showVisibility()
+        }
+        linearLayout.toot_card_vote_button.setOnClickListener {
+            showVote()
+        }
+        linearLayout.toot_card_scheduled_time_button.setOnClickListener {
+            showScheduled()
+        }
+        linearLayout.toot_card_paint_post_button.setOnClickListener {
+            showPaint()
+        }
+    }
+
     fun setAttachImage(){
         
     }
@@ -104,11 +130,28 @@ class TootCardView(val context: Context,val isMisskey:Boolean){
     }
     
     fun showScheduled(){
-        
+        if(linearLayout.toot_card_scheduled_linearlayout.visibility== View.GONE){
+            linearLayout.toot_card_scheduled_linearlayout.visibility == View.VISIBLE
+        }else{
+            linearLayout.toot_card_scheduled_linearlayout.visibility == View.GONE
+        }
+        linearLayout.toot_card_scheduled_time_button.setOnClickListener {
+            //時間ピッカー
+            showTimePicker(linearLayout.toot_card_scheduled_time_textview)
+        }
+        linearLayout.toot_card_scheduled_date_button.setOnClickListener {
+            //日付ピッカー
+            showDatePicker(linearLayout.toot_card_scheduled_date_textview)
+        }
+
     }
     
     fun showVote(){
-        
+        if(linearLayout.toot_card_vote_linearlayout.visibility== View.GONE){
+            linearLayout.toot_card_vote_linearlayout.visibility == View.VISIBLE
+        }else{
+            linearLayout.toot_card_vote_linearlayout.visibility == View.GONE
+        }
     }
     
     fun showPaint(){
@@ -118,7 +161,65 @@ class TootCardView(val context: Context,val isMisskey:Boolean){
     fun postStatus(){
         
     }
-    
+
+
+    /**
+     * DatePicker
+     */
+    private fun showDatePicker(textView: TextView) {
+        val date = arrayOf("")
+
+        val calendar = Calendar.getInstance()
+        val dateBuilder = DatePickerDialog(context, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            var month = month
+            var month_string = ""
+            var day_string = ""
+            //1-9月は前に0を入れる
+            if (month++ <= 9) {
+                month_string = "0" + month++.toString()
+            } else {
+                month_string = month++.toString()
+            }
+            //1-9日も前に0を入れる
+            if (dayOfMonth <= 9) {
+                day_string = "0$dayOfMonth"
+            } else {
+                day_string = dayOfMonth.toString()
+            }
+            textView.text = year.toString() + month_string + day_string + "T"
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        dateBuilder.show()
+    }
+
+    /**
+     * TimePicker
+     */
+    private fun showTimePicker(textView: TextView) {
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+        val dialog = TimePickerDialog(context, TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+            var hourOfDay = hourOfDay
+            var hour_string = ""
+            var minute_string = ""
+            //1-9月は前に0を入れる
+            if (hourOfDay <= 9) {
+                hour_string = "0" + hourOfDay++.toString()
+            } else {
+                hour_string = hourOfDay++.toString()
+            }
+            //1-9日も前に0を入れる
+            if (minute <= 9) {
+                minute_string = "0$minute"
+            } else {
+                minute_string = minute.toString()
+            }
+            textView.text = hour_string + minute_string + "00" + "+0900"
+        }, hour, minute, true)
+        dialog.show()
+    }
+
 
 
 }
